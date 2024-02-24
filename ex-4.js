adae683b1925b84f9af912b28944ec2b1e9b9f1e
Jirayu <kraiwilat@gmail.com>
2024-02-24 12:04:52 +0700

    fe2-Chit-Zong-Nonae-gap

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,4 @@
-// Exercise #4
-let getJohnProfile = () => {
+let getJohnProfile1 = () => {
   return new Promise(function (resolve) {
     setTimeout(
       () =>
@@ -12,4 +11,22 @@ let getJohnProfile = () => {
     );
   });
 };
-// Start coding here
+
+const getJohnProfile2 = async () => {
+  try {
+    const profile = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          name: "John",
+          age: 20,
+          hobbies: ["Coding", "Football"],
+        });
+      }, 1000);
+    });
+    console.log(profile);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+getJohnProfile2();
--- ex-5.js
+++ ex-5.js
@@ -12,3 +12,21 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+
+const getJohnProfileRejectedData = async () => {
+  try {
+    const rejectedData = await new Promise((_, reject) => {
+      setTimeout(() => {
+        reject({
+          errorCode: 500,
+          message: "👿 Failed to request data from server",
+        });
+      }, 2000);
+    });
+    console.log(rejectedData);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getJohnProfileRejectedData();
